2ca01f0a0db558683be4dd34f771f225d4eee171
MateusViniMelo <mateusmelo04@gmail.com>
2024-10-16 09:09:21 -0300

    fix: fazer o link para exibir as imagens

--- .fly/fix_permissions.sh
+++ .fly/fix_permissions.sh
@@ -8,5 +8,9 @@ chmod -R 775 /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/bootstrap/cache
 
+
+# Rodar o comando php artisan storage:link
+php /var/www/html/artisan storage:link
+
 # Executar o comando original do entrypoint
 exec "$@"
